37e3dd6af16be94cb2f703f63b507169df2e464b
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2020-12-18 01:00:21 +0300

    Uri is shown

--- app/src/main/java/com/alexandr7035/silentvideo/MainActivity.kt
+++ app/src/main/java/com/alexandr7035/silentvideo/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.alexandr7035.silentvideo
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
+    fun chooseFileBtn(v: View) {
+        val intent: Intent
+        val chooseFile = Intent(Intent.ACTION_GET_CONTENT)
+
+        chooseFile.type = "*/*"
+        intent = Intent.createChooser(chooseFile, "Choose a file")
+        startActivityForResult(intent, 1)
+
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            val selectedFile = data?.data
+
+            videoPreview.text = selectedFile.toString()
+
+        }
+    }
+
+
 }
