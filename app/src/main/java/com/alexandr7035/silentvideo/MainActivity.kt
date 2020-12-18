b00d098d4c84fa9544af9d143a4fecda101ed879
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2020-12-18 17:40:50 +0300

    Add LiveData for observing uri

--- app/src/main/java/com/alexandr7035/silentvideo/MainActivity.kt
+++ app/src/main/java/com/alexandr7035/silentvideo/MainActivity.kt
@@ -2,14 +2,29 @@ package com.alexandr7035.silentvideo
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import kotlinx.android.synthetic.main.activity_main.*
+
+private val LOG_TAG = "DEBUG_SV"
+private lateinit var videoUriLiveData: MutableLiveData<String>
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        videoUriLiveData = MutableLiveData()
+
+        videoUriLiveData.observe(this, Observer<String> { uri ->
+            if (uri != null ) {
+                videoPreview.text = uri
+            }
+        })
+
     }
 
 
@@ -21,16 +36,18 @@ class MainActivity : AppCompatActivity() {
         intent = Intent.createChooser(chooseFile, "Choose a file")
         startActivityForResult(intent, 1)
 
-
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
         if (requestCode == 1 && resultCode == RESULT_OK) {
+
+            Log.d(LOG_TAG, "update video uri")
+
             val selectedFile = data?.data
 
-            videoPreview.text = selectedFile.toString()
+            videoUriLiveData.postValue(selectedFile.toString())
 
         }
     }
